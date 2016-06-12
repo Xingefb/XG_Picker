a3fd5f7a7c88da88e979cbcd04d89f1968b669a8
Xingefb <18730217732@163.com>
2016-06-12 11:08:28 +0800

    相机相册 图片选择器 支持相机连拍

---
+++ XG_Picker/XG_Picker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XG_Picker
+//
+//  Created by XG on 16/6/12.
+//  Copyright © 2016年 com.Xg_FB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XG_Picker/XG_Picker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XG_Picker
+//
+//  Created by XG on 16/6/12.
+//  Copyright © 2016年 com.Xg_FB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
